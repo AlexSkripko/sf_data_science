7907bbd04fbbc4a0819ea1c1917b67992b046eaa
Wreed <wreed@mail.ru>
2022-11-17 21:17:18 +0300

    added readme

--- guess_number_task/game_v2.py
+++ guess_number_task/game_v2.py
@@ -4,8 +4,7 @@
 
 import numpy as np
 
-
-def random_predict(number: int = 1) -> int:
+def random_predict(number: int = 50) -> int:
     """Рандомно угадываем число
 
     Args:
@@ -15,12 +14,20 @@ def random_predict(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
+    start_number = 1
+    last_number = 100
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла если угадали
+        predict_number = np.random.randint(start_number, last_number + 1)  #предполагаемое число
+       
+        if number == predict_number: # если угадали, то выходим из цикла
+            break
+        if predict_number > number:
+            last_number = predict_number # переопределяем конец рассматриваемого множества
+        else:
+            start_number = predict_number # переопределяем начало конец рассматриваемого множества
+   
     return count
 
 
@@ -35,7 +42,7 @@ def score_game(random_predict) -> int:
     """
     count_ls = []
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
